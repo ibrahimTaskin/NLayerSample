9f56a0dfc6349b76b4a3558ab976e67b92a807e3
ibrahimTaskin <46849933+ibrahimTaskin@users.noreply.github.com>
2019-01-30 23:33:15 +0300

    Add files via upload

---
+++ NLayer.BLL/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using NLayer.DAL.Abstract;
+using NLayer.DAL.Concrete;
+using NLayer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.BLL.Concrete
+{
+    /*işlemlerimizi iş katmanında yaparız.
+    Veritabanında gelen listeler/veriler aşağıdaki şartlara uyuyor mu?
+    Kuralları bu katmanda belirleriz.*/
+    public class ProductManager
+    {
+        private IProductDal _productDal;
+
+        //dependency injection ile new yapmadan enjekte ediyoruz.
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //İş kuralları ile ilgili kodlar buraya yazılır.
+            return _productDal.GetAll();
+        }
+    }
+}
